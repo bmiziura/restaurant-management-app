c1299621c9d4df428731d248048b15bf690ec21c
Eryczal <79263592+Eryczal@users.noreply.github.com>
2023-12-17 19:12:06 +0100

    #5 Add Header

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,10 +1,22 @@
-import { Link } from "react-router-dom"
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
+import { Link, Outlet } from "react-router-dom"
 
 function HomePage() {
   return (
     <div>
       <Link to="/login">Zaloguj się</Link>
     </div>
+  )
+}
+
+export const HomePageLayout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
   )
 }
 
