93d837da310be412720eb8243db0b4ec94f83e8e
Eryczal <79263592+Eryczal@users.noreply.github.com>
2023-12-17 19:18:37 +0100

    #5 Merge Div into Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,23 +2,21 @@ import { Button } from "./ui/button"
 
 function Header() {
   return (
-    <header className="h-20 container">
-      <div className="flex h-full justify-between items-center">
-        <h1>Restaurant Management</h1>
-        <nav>
-          <ul className="flex text-lg gap-x-4 items-center">
-            <li>Cennik</li>
-            <li>Opinie</li>
-            <li>O nas</li>
-            <li>Kontakt</li>
-            <li>
-              <Button className="text-sm font-bold uppercase bg-slate-800 text-slate-100">
-                Zaloguj
-              </Button>
-            </li>
-          </ul>
-        </nav>
-      </div>
+    <header className="h-20 container flex justify-between items-center">
+      <h1>Restaurant Management</h1>
+      <nav>
+        <ul className="flex text-lg gap-x-4 items-center">
+          <li>Cennik</li>
+          <li>Opinie</li>
+          <li>O nas</li>
+          <li>Kontakt</li>
+          <li>
+            <Button className="text-sm font-bold uppercase bg-slate-800 text-slate-100">
+              Zaloguj
+            </Button>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
