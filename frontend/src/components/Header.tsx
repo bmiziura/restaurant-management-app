15465a3d91152d9ac07f55a98e3acdf2b92c27e0
Eryczal <79263592+Eryczal@users.noreply.github.com>
2023-12-19 16:43:28 +0100

    Add Links to Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,12 @@
 import { Button } from "./ui/button"
+import { Link } from "react-router-dom"
 
 function Header() {
   return (
     <header className="h-20 container flex justify-between items-center">
-      <h1>Restaurant Management</h1>
+      <h1>
+        <Link to="/">Restaurant Management</Link>
+      </h1>
       <nav className="flex gap-x-4">
         <ul className="gap-x-4 items-center hidden md:flex">
           <li>Cennik</li>
@@ -11,9 +14,11 @@ function Header() {
           <li>O nas</li>
           <li>Kontakt</li>
         </ul>
-        <Button className="text-sm font-bold uppercase bg-slate-800 text-slate-100">
-          Zaloguj
-        </Button>
+        <Link to="/login">
+          <Button className="text-sm font-bold uppercase bg-slate-800 text-slate-100">
+            Zaloguj
+          </Button>
+        </Link>
       </nav>
     </header>
   )
