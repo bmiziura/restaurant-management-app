8e894efab07393aa6bf17cb1e6c0121cea1cf0c2
Eryczal <79263592+Eryczal@users.noreply.github.com>
2023-12-19 16:50:08 +0100

    Add Form to Login Page

--- frontend/src/pages/AuthPage.tsx
+++ frontend/src/pages/AuthPage.tsx
@@ -16,38 +16,42 @@ function AuthPage() {
   return (
     <>
       <div className="container text-center max-w-xs md:py-64">
-        <h2 className="text-2xl mb-4">Zaloguj się</h2>
-        <div className="relative mt-6">
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder=""
-            className="peer absolute top-0 left-0 w-full border-2 rounded-xl p-2 border-slate-100 focus:outline-none focus:border-2 focus:border-sky-500"
-          />
-          <label
-            htmlFor="email"
-            className="transition-all absolute p-2 top-0 left-0 z-10 peer-focus:text-sky-500 peer-focus:-top-8 peer-focus:text-sm peer-[:not(:placeholder-shown)]:-top-8 peer-[:not(:placeholder-shown)]:text-sm"
-          >
-            Email
-          </label>
-        </div>
-        <div className="relative mt-24">
-          <input
-            type="password"
-            id="password"
-            name="password"
-            placeholder=""
-            className="peer absolute top-0 left-0 w-full border-2 rounded-xl p-2 border-slate-100 focus:outline-none focus:border-2 focus:border-sky-500"
-          />
-          <label
-            htmlFor="password"
-            className="transition-all absolute p-2 top-0 left-0 z-10 peer-focus:text-sky-500 peer-focus:-top-8 peer-focus:text-sm peer-[:not(:placeholder-shown)]:-top-8 peer-[:not(:placeholder-shown)]:text-sm"
-          >
-            Hasło
-          </label>
-        </div>
-        <Button className="mt-[4.5rem]">Zaloguj się</Button>
+        <form onSubmit={handleLogin}>
+          <h2 className="text-2xl mb-4">Zaloguj się</h2>
+          <div className="relative mt-6">
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder=""
+              className="peer absolute top-0 left-0 w-full border-2 rounded-xl p-2 border-slate-100 focus:outline-none focus:border-2 focus:border-sky-500"
+            />
+            <label
+              htmlFor="email"
+              className="transition-all absolute p-2 top-0 left-0 z-10 peer-focus:text-sky-500 peer-focus:-top-8 peer-focus:text-sm peer-[:not(:placeholder-shown)]:-top-8 peer-[:not(:placeholder-shown)]:text-sm"
+            >
+              Email
+            </label>
+          </div>
+          <div className="relative mt-24">
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder=""
+              className="peer absolute top-0 left-0 w-full border-2 rounded-xl p-2 border-slate-100 focus:outline-none focus:border-2 focus:border-sky-500"
+            />
+            <label
+              htmlFor="password"
+              className="transition-all absolute p-2 top-0 left-0 z-10 peer-focus:text-sky-500 peer-focus:-top-8 peer-focus:text-sm peer-[:not(:placeholder-shown)]:-top-8 peer-[:not(:placeholder-shown)]:text-sm"
+            >
+              Hasło
+            </label>
+          </div>
+          <Button type="submit" className="mt-[4.5rem]">
+            Zaloguj się
+          </Button>
+        </form>
         <p className="mt-2">
           Nie masz konta?{" "}
           <Link to="/register" className="text-sky-700">
